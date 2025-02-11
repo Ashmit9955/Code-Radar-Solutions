dbd708d7e0332d008b9722ff04d666b52ebf759f
Ashmit9955 <ashmit9955@gmail.com>
2025-02-12 04:16:07 +0530

    Solution to Simple Calculator

--- Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
+++ Simple Calculator@@6788dd63490637cfc44e1ba8/code.c
@@ -3,7 +3,7 @@ int main(){
     double x,y;
     char z;
     scanf("%lf%lf%c",&x,&y,&z);
-    printf("%lf%lf&c",x,y,z);
+    printf("%lf%lf%c",x,y,z);
     switch(z){
         case '+':
         printf("%lf",x+y);
