f46d7c32da7d47353ff7e9a442a3445afd50a421
Ashmit9955 <ashmit9955@gmail.com>
2025-03-18 23:15:17 +0530

    Solution to Find the Pair with the Minimum Difference

--- Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
+++ Find the Pair with the Minimum Difference@@676c02002dbc8c3e7a280b88/code.c
@@ -1,1 +1,27 @@
-// A better approach is to first sort the array and then get the min diff pair
+#include <stdio.h>
+int main(){
+    int n;
+    scanf("%d",&n);
+    int arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%d",&arr[i]);
+    }
+    int Ssum = 0;
+    int num1 = -2;
+    int num2 = -2;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<n; j++){
+            if(i == j){
+                continue;
+            }else if(i != j){
+                if((arr[i] - arr[j]) < Ssum){
+                    Ssum = arr[i] - arr[j];
+                    num1 = i;
+                    num2 = j;
+                }
+            }
+        }
+    }
+    printf("%d %d",arr[num1],arr[num2]);
+    return 0;
+}
