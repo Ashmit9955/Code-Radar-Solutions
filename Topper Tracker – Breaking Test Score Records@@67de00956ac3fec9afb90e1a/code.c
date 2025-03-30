f5a83e69ee9ef7117de81ea3a0876adaea29ee5c
Ashmit9955 <ashmit9955@gmail.com>
2025-03-30 22:49:51 +0530

    Solution to Topper Tracker – Breaking Test Score Records

--- Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
+++ Topper Tracker – Breaking Test Score Records@@67de00956ac3fec9afb90e1a/code.c
@@ -1,24 +1,22 @@
 #include<stdio.h>
-int main(){
-    int n;
-    scanf("%d",&n);
-    int arr[n];
-    for(int i=0; i<n; i++){
-        scanf("%d",&arr[i]);
+
+void trackscores(int scores[];int n; int result[]){
+      for(int i=0; i<n; i++){
+        scanf("%d",&scores[i]);
     }
-    int HS = arr[0];
+    int HS = scores[0];
     int H = 0;
-    int LS = arr[0];
+    int LS = scores[0];
     int L = 0;
     for(int i=1; i<n; i++){
         if(arr[i] > HS){
-            HS = arr[i];
+            HS = scores[i];
             H++;
-        }else if(arr[i] < LS){
-            LS = arr[i];
+        }else if(scores[i] < LS){
+            LS = scores[i];
             L++;
         }
-    }
-    printf("%d %d",H,L);
-    return 0;
+}
+result[0] = H;
+result[1] = L;
 }
