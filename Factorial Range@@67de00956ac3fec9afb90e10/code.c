ede60bc481ad8e3f6fdad555f2b8ae0e2cc3939b
Ashmit9955 <ashmit9955@gmail.com>
2025-03-31 18:28:40 +0530

    Solution to Factorial Range

---
+++ Factorial Range@@67de00956ac3fec9afb90e10/code.c
@@ -0,0 +1,9 @@
+void factorialRange(int n, int m){
+    for(int i=n; i<=m; i++){
+        int fact = 1;
+        for(int j=1;j<=i;j++){
+            fact = fact*j;
+        }
+        printf("%d\n",fact);
+    }
+}
