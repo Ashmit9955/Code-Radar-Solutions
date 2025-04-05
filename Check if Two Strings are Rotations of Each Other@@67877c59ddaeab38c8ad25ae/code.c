1da9748d29286004e243298f3f66b6372c23ef1f
Ashmit9955 <ashmit9955@gmail.com>
2025-04-06 02:19:16 +0530

    Solution to Check if Two Strings are Rotations of Each Other

--- Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
+++ Check if Two Strings are Rotations of Each Other@@67877c59ddaeab38c8ad25ae/code.c
@@ -1,23 +1,35 @@
-#include<stdio.h>
-#include<string.h>
-int main(){
+#include <stdio.h>
+#include <string.h>
+
+int main() {
     char ch[20];
     char ch1[20];
-    fgets(ch,sizeof(ch),stdin);
-    fgets(ch1,sizeof(ch1),stdin);
-    ch[strcspn(ch, "\n")] = '\0';
-    ch1[strcspn(ch1, "\n")] = '\0';
+
+    fgets(ch, sizeof(ch), stdin);
+    fgets(ch1, sizeof(ch1), stdin);
+
+    // Handle both \n and \r\n line endings
+    ch[strcspn(ch, "\r\n")] = '\0';
+    ch1[strcspn(ch1, "\r\n")] = '\0';
 
     int n = strlen(ch);
-    for(int i=0; i< n/2;i++){
+
+    // Reverse ch in place
+    for (int i = 0; i < n / 2; i++) {
         char temp = ch[i];
-        ch[i] = ch[n-1-i];
-        ch[n-1-i] = temp;
+        ch[i] = ch[n - 1 - i];
+        ch[n - 1 - i] = temp;
     }
-    if(strcmp(ch,ch1) == 0){
-        printf("Yes");
-    }else{
-        printf("No");
+
+    // Debug output
+    printf("Reversed ch: %s\n", ch);
+    printf("Original ch1: %s\n", ch1);
+
+    if (strcmp(ch, ch1) == 0) {
+        printf("Yes\n");
+    } else {
+        printf("No\n");
     }
+
     return 0;
 }
