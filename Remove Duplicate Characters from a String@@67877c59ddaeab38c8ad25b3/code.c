3c3bc6cc1ec673f8791fa36dcf431af08bf4841b
Ashmit9955 <ashmit9955@gmail.com>
2025-04-06 16:13:35 +0530

    Solution to Remove Duplicate Characters from a String

---
+++ Remove Duplicate Characters from a String@@67877c59ddaeab38c8ad25b3/code.c
@@ -0,0 +1,29 @@
+#include<stdio.h>
+#include<string.h>
+#include<ctype.h>
+
+void fun(char* ch){
+    char str[40];
+    int num = 0;
+    int n = strlen(ch);
+    for(int i=0; i<n;i++){
+        int count = 0;
+        for(int j=0 ;j<n;j++){
+            if(ch[i] == ch[j]){
+                count++;
+            }
+        }
+        if(count == 1){
+            str[num++] = ch[i];
+        }
+    }
+    printf("%s",str);
+}
+
+int main(){
+    char ch[40];
+    fgets(ch,sizeof(ch),stdin);
+    ch[strcspn(ch,"\n")] = '\0';
+    fun(ch);
+    return 0;
+}
