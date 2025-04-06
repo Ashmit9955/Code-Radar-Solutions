1c1d9ea461c72341cd83959d18a8235aa2341e70
Ashmit9955 <ashmit9955@gmail.com>
2025-04-06 14:47:36 +0530

    Solution to Check if a String is Binary

--- Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
+++ Check if a String is Binary@@67877c59ddaeab38c8ad25b6/code.c
@@ -5,9 +5,9 @@ int main(){
     fgets(ch,sizeof(ch),stdin);
     ch[strcspn(ch,"\n")] == '\0';
     int n = strlen(ch);
-    int result = 0;
+    int result = 1;
     for(int i=0; i<n; i++){
-        if(ch[i] == 1 || ch[i] == 0){
+        if(ch[i] == '1' || ch[i] == '0'){
             result = 1;
         }else{
             result = 0;
