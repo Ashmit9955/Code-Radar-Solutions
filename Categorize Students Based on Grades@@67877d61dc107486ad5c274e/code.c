86fa5c9ceb0a5b4e7db639eb3a9b6557799d3e19
Ashmit9955 <ashmit9955@gmail.com>
2025-04-08 19:55:22 +0530

    Solution to Categorize Students Based on Grades

--- Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
+++ Categorize Students Based on Grades@@67877d61dc107486ad5c274e/code.c
@@ -10,7 +10,7 @@ int main(){
     }Student;
     Student arr[n];
     for(int i=0; i<n; i++){
-        scanf("%d",arr[i].roll);
+        scanf("%d",&arr[i].roll);
         scanf("%s",arr[i].name);
         scanf("%f",&arr[i].marks);
     }
