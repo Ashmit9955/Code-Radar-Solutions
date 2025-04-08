5a10965ba4fc4626fcf4a2305a1fe4af9f1c95a4
Ashmit9955 <ashmit9955@gmail.com>
2025-04-08 23:26:49 +0530

    Solution to Manage Book Library

--- Manage Book Library@@67877d61dc107486ad5c274c/code.c
+++ Manage Book Library@@67877d61dc107486ad5c274c/code.c
@@ -5,7 +5,21 @@ int main(){
     scanf("%d",&n);
     typedef struct Book{
         char book[20];
-        char author
+        char author[20];
+        float price;
+    }Book;
+    Book arr[n];
+    for(int i=0; i<n; i++){
+        scanf("%s",arr[i].book);
+        scanf("%s",arr[i].author);
+        scanf("%f",&arr[i].price);
+    }
+    float target;
+    scanf("%f",&target);
+    for(int i=0; i<n; i++){
+        if(arr[i].price>target){
+            printf("Title: %s, Author: %s, Price: %.2f\n",arr[i].book,arr[i].author,arr[i].price);
+        }
     }
     return 0;
 }
